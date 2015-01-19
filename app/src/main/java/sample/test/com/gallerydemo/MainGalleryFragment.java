3cc755e345666585821127faba5d15fa3ef789cb
Nasif <nasifn@gmail.com>
2015-01-19 21:39:00 +0530

    Added seperate Fragment

---
+++ app/src/main/java/sample/test/com/gallerydemo/MainGalleryFragment.java
@@ -0,0 +1,44 @@
+package sample.test.com.gallerydemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Nasif on 18-Jan-15.
+ */
+
+
+
+
+public  class MainGalleryFragment extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+    public static MainGalleryFragment newInstance(int sectionNumber) {
+        MainGalleryFragment fragment = new MainGalleryFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public MainGalleryFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((MainActivity) activity).onSectionAttached(
+                getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+}
